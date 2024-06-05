4e338395151b0785161db12f332f316ebe187d2e
sonkey <annagu.kennedy@gmail.com>
2024-06-05 15:12:41 +0100

    cors

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
 const bodyParser = require("body-parser");
-const { config } = require("dotenv");
+const { configDotenv } = require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
 const app = express();
 const port = 3000;
 
-config(); // Correct way to load environment variables
-
-const pass = process.env.PASS; // Use uppercase for environment variable names
-const user = process.env.USER;
+const pass = process.env.pass;
+const user = process.env.user;
 
 const transporter = nodemailer.createTransport({
+  service: "SMTP",
   host: "mail.privateemail.com",
-  port: 465,
-  secure: true, // Use SSL
+  port: "465",
   auth: {
     user: user,
     pass: pass,
   },
 });
 
-// Enable CORS for all routes
 app.use(cors());
+
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get("/", function (req, res) {
-  res.send("Welcome to the email sending service");
+  res.send(req.body);
 });
 
 app.post("/email", function (req, res) {
   const { text } = req.body;
   const mailOptions = {
     from: "app@wallstreetmeme.co",
-    to: "annagu.kennedy@gmail.com", // Ideally, use an environment variable or request data
-    subject: "Nodemailer",
+    to: "annagu.kennedy@gmail.com",
+    subject: " Nodemailer",
     text,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.error(error.message);
+      console.log(error.message);
       res.status(500).json({ message: error.message });
     } else {
       console.log("Email sent: " + info.response);
-      res.status(200).json({ message: "Email sent successfully" });
+      //res.redirect("https://wallstreetmeme.co/404.html");
+      res.end();
     }
   });
 });
 
 app.listen(port, function () {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listenig on port ${port}`);
 });
